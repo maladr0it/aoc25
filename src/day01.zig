afdb91aeec2de693bc1fd3615300ea7e179442bf
maladr0it <bradford.lachlan@gmail.com>
2026-01-01 06:55:56 +0900

    day1p2

--- src/day01.zig
+++ src/day01.zig
@@ -2,7 +2,6 @@
 
 const std = @import("std");
 
-// const data = @embedFile("data/x.txt");
 const data = @embedFile("data/day01.txt");
 
 const START_POS = 50;
@@ -28,6 +27,31 @@ pub fn part1() usize {
         if (pos == 0) {
             zero_count += 1;
         }
+    }
+
+    return zero_count;
+}
+
+pub fn part2() usize {
+    var zero_count: usize = 0;
+    var pos: i16 = START_POS;
+
+    var lines = std.mem.tokenizeScalar(u8, data, '\n');
+    while (lines.next()) |line| {
+        const dir: i16 = switch (line[0]) {
+            'L' => -1,
+            'R' => 1,
+            else => unreachable,
+        };
+        const num_steps = std.fmt.parseInt(i16, line[1..], 10) catch unreachable;
+
+        const dist_to_zero = @mod(dir * -pos - 1, VALUES_COUNT) + 1;
+        if (num_steps >= dist_to_zero) {
+            const extra_steps: usize = @intCast(num_steps - dist_to_zero);
+            zero_count += 1 + extra_steps / VALUES_COUNT;
+        }
+
+        pos = @mod(pos + (dir * num_steps), VALUES_COUNT);
     }
 
     return zero_count;
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
-const day1 = @import("day01.zig");
-
 const print = std.debug.print;
+
+const day1 = @import("day01.zig");
 
 pub fn main() !void {
     print("day 1 part 1:\n{any}\n", .{day1.part1()});
+    print("day 1 part 2:\n{any}\n", .{day1.part2()});
 }

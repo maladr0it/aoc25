29dd9c7743c4279629f7e8759a56342fd95718e7
maladr0it <bradford.lachlan@gmail.com>
2026-01-04 20:43:09 -0800

    clean up

--- src/main.zig
+++ src/main.zig
@@ -20,16 +20,17 @@ pub fn stop_timer() void {
 
 pub fn main() !void {
     start_timer();
-    if (false) {
+    if (true) {
         print("Day 01 Part 1: {d}\n", .{day1.part1()}); // 1132
         print("Day 01 Part 2: {d}\n", .{day1.part2()}); // 6623
         print("Day 02 Part 1: {d}\n", .{day2.part1()}); // 31000881061
         print("Day 02 Part 2: {d}\n", .{day2.part2()}); // CLEAN THIS ONE UP 46769308485
         print("Day 03 Part 1: {d}\n", .{day3.part1()}); // 17107
         print("Day 03 Part 1: {d}\n", .{day3.part2()}); // 169349762274117
-        print("Day 04 Part 1: {d}\n", .{day4.part1()}); // 1495
-        print("Day 04 Part 2: {d}\n", .{day4.part2()}); // 8768
+
     }
+    print("Day 04 Part 1: {d}\n", .{day4.part1()}); // 1495
+    print("Day 04 Part 2: {d}\n", .{day4.part2()}); // 8768
 
     stop_timer();
 }
